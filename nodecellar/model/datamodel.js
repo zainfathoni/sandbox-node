dab65729e793db7cfcd8b30c9bb2adc2122b094c
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-02 15:12:01 +0700

    [Modify] findById() using collection.findOne()

--- nodecellar/model/datamodel.js
+++ nodecellar/model/datamodel.js
@@ -6,7 +6,7 @@ var url = 'mongodb://localhost:27017/test';
 
 exports.dbTest = function(req, res) {
     MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
+        assert.equal(null, err); // for unit testing
         res.send({ status: 'OK', message: 'Connected correctly to server.' });
         db.close();
     });
@@ -16,7 +16,7 @@ exports.insert = function(db, obj, callback) {
     db.collection('restaurants').insertOne(
         obj,
         function(err, result) {
-            assert.equal(err, null);
+            assert.equal(err, null); // for unit testing
             callback({ status: 'OK', message: 'Inserted a document into the restaurants collection.' });
         });
 };
@@ -24,21 +24,17 @@ exports.insert = function(db, obj, callback) {
 exports.findAll = function(db, callback) {
     var cursor = db.collection('restaurants').find();
     cursor.toArray(function(err, restaurants) {
-        assert.equal(err, null);
+        assert.equal(err, null); // for unit testing
         callback(restaurants);
     });
 };
 
 exports.findById = function(db, id, callback) {
-    var cursor = db.collection('restaurants').find({
-        "_id": new ObjectId(id)
-    });
-    if (cursor.hasNext()) {
-        cursor.next(function(err, item) {
+    db.collection('restaurants').findOne(
+        {'_id': new ObjectId(id)},
+        function(err, item) {
             assert.equal(err, null);
             callback(item);
-        });
-    } else {
-        callback({});
-    }
+        }
+    );
 };
