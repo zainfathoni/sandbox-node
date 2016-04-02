257d94d8b09eb62af72e77b9d9981a87422e7b9c
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-02 09:07:17 +0700

    [Modify] DataModel Abstractions

--- nodecellar/model/datamodel.js
+++ nodecellar/model/datamodel.js
@@ -7,7 +7,17 @@ var url = 'mongodb://localhost:27017/test';
 exports.dbTest = function(req, res) {
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        res.send({status:'ok',message:'Connected correctly to server.'});
+        res.send({ status: 'ok', message: 'Connected correctly to server.' });
         db.close();
     });
 }
+
+exports.insert = function(db, obj, callback) {
+    db.collection('restaurants').insertOne(
+        obj,
+        function(err, result) {
+            assert.equal(err, null);
+            console.log("Inserted a document into the restaurants collection.");
+            callback();
+        });
+};
--- nodecellar/routes/restaurants.js
+++ nodecellar/routes/restaurants.js
@@ -3,34 +3,25 @@ var MongoClient = mongo.MongoClient;
 var ObjectId = mongo.ObjectID;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/test';
+var datamodel = require('../model/datamodel');
 
 exports.findAll = function(req, res) {
-  res.send([{ name: 'restaurant1' }, { name: 'restaurant2' }, { name: 'restaurant3' }]);
+    res.send([{ name: 'restaurant1' }, { name: 'restaurant2' }, { name: 'restaurant3' }]);
 };
 
 exports.findById = function(req, res) {
-  res.send({ id: req.params.id, name: "The Name", description: "description" });
+    res.send({ id: req.params.id, name: "The Name", description: "description" });
 };
 
 exports.add = function(req, res) {
-  var restaurants = req.body;
-  console.log('Adding restaurants: ' + JSON.stringify(restaurants));
+    var restaurants = req.body;
+    console.log('Adding restaurants: ' + JSON.stringify(restaurants));
 
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    insertDocument(db, restaurants, function() {
-      db.close();
-      res.send(restaurants)
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        datamodel.insert(db, restaurants, function() {
+            db.close();
+            res.send(restaurants)
+        });
     });
-  });
 }
-
-var insertDocument = function(db, obj, callback) {
-  db.collection('restaurants').insertOne(
-    obj,
-    function(err, result) {
-      assert.equal(err, null);
-      console.log("Inserted a document into the restaurants collection.");
-      callback();
-    });
-};
