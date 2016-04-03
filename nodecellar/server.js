53e04bcc3aff5593c68c28cc090424fadf0b3614
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-03 12:04:34 +0700

    [Refactor] using express.js 4.x

--- nodecellar/server.js
+++ nodecellar/server.js
@@ -1,36 +1,37 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var restaurants = require('./routes/restaurants');
 var datamodel = require('./model/datamodel');
 
 // Initialization
 var app = express();
+var port = process.env.PORT || 3000;
 
-// Configuration
-app.configure(function() {
-    // Logger: 'default', 'short', 'tiny', 'dev'
-    app.use(express.logger('dev'));
+// CONFIGURATION
+// Morgan Logger
+app.use(morgan('combined'));
 
-    // Body Parser
-    app.use(express.bodyParser());
+// Body Parser
+app.use(bodyParser.json());
 
-    // Development error handler
-    if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        });
-    }
-
-    // Production Error Handler
+// Development error handler
+if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: {}
+            error: err
         });
+    });
+}
+
+// Production Error Handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
 });
 
@@ -41,6 +42,23 @@ app.get('/restaurants/:id', restaurants.findById);
 app.put('/restaurants/:id', restaurants.update);
 app.delete('/restaurants/:id', restaurants.delete);
 
-// Listening
-app.listen(3000);
-console.log('Listening on port 3000...');
+// ROUTES
+// Get an instance of router
+var router = express.Router();
+
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+    res.send('im the home page!');
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+    res.send('im the about page!');
+});
+
+// Apply Routes
+app.use('/', router);
+
+// START SERVER
+app.listen(port);
+console.log('Listening on port ' + port);
