60f779cf4f6296f3ca6e0e56f92cd4c425adbb48
Zain Fathoni <zain.fathoni@gmail.com>
2016-04-03 12:32:07 +0700

    [Add] Usage samples of express route

--- nodecellar/server.js
+++ nodecellar/server.js
@@ -46,6 +46,16 @@ app.delete('/restaurants/:id', restaurants.delete);
 // Get an instance of router
 var router = express.Router();
 
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next(); 
+});
+
 // home page route (http://localhost:8080)
 router.get('/', function(req, res) {
     res.send('im the home page!');
@@ -55,6 +65,37 @@ router.get('/', function(req, res) {
 router.get('/about', function(req, res) {
     res.send('im the about page!');
 });
+
+// route middleware to validate :name
+router.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // blah blah validation
+    // log something so we know its working
+    console.log('doing name validations on ' + name);
+
+    // once validation is done save the new item in the req
+    req.name = name;
+    // go to the next thing
+    next(); 
+});
+
+// route with parameters (http://localhost:8080/hello/:name)
+router.get('/hello/:name', function(req, res) {
+    res.send('hello ' + req.name + '!');
+});
+
+app.route('/login')
+
+    // show the form (GET http://localhost:8080/login)
+    .get(function(req, res) {
+        res.send('this is the login form');
+    })
+
+    // process the form (POST http://localhost:8080/login)
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
 
 // Apply Routes
 app.use('/', router);
